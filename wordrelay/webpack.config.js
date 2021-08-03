f5cfd3d1e585b5ec60238506c347836899a0ec13
geena-keem <geena.gamm@gmail.com>
2021-08-03 20:59:51 +0900

    add wordrelay: build with webpack

--- wordrelay/webpack.config.js
+++ wordrelay/webpack.config.js
@@ -12,6 +12,22 @@ module.exports = {
   entry: {
     app: ['./client'],
   }, // 입력
+
+  // entry의 파일을 읽고 모듈을 적용한 후 output으로 뺀다.
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // js나 jsx파일을
+        loader: 'babel-loader', // 바벨 로더을 적용해서 문법을 호환되게끔 바꿔준다.
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          // plugins: ['@babel/plugin-proposal-class-properties'],
+          // 강의는 에러나서 이거 추가함 (npm i -D @babel/plugin-proposal-class-properties)
+        },
+      },
+    ],
+  },
+
   output: {
     path: path.join(__dirname, 'dist'), // 현재 폴더 경로인 dist폴더
     filename: 'app.js',
